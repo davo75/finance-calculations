c196494188bc1eb5040191724d7456035a244dfe
David Pyle <david@scancam.com.au>
2018-10-31 12:22:31 +0800

    Added feature to save calculationsto txt file and option to read the file

--- Finance/Finance.Client/FinanceUI.cs
+++ Finance/Finance.Client/FinanceUI.cs
@@ -1,5 +1,7 @@
 ﻿using Finance.Library;
 using System;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Finance.Client
 {
@@ -24,19 +26,19 @@ namespace Finance.Client
                     double loanAmount;
                     do
                     {
-                        Console.WriteLine("Enter the loan amount:");
+                        Console.WriteLine("Enter the loan amount in dollars (e.g. 10000):");
                     } while (!Double.TryParse(Console.ReadLine(), out loanAmount) || loanAmount <= 0);
 
                     double interestRate;
                     do
                     {
-                        Console.WriteLine("Enter interest rate (%):");
+                        Console.WriteLine("Enter interest rate as a percentage (e.g. 5.2):");
                     } while (!Double.TryParse(Console.ReadLine(), out interestRate) || interestRate <= 0);
 
                     int loanTermInYears;
                     do
                     {
-                        Console.WriteLine("Enter the term of the loan in years:");
+                        Console.WriteLine("Enter the term of the loan in years (e.g. 5):");
                     } while (!Int32.TryParse(Console.ReadLine(), out loanTermInYears) || loanTermInYears <= 0);
 
                     //create new calculator object to call repayment method
@@ -48,6 +50,15 @@ namespace Finance.Client
                         double monthlyRepayment = calculator.GetRepayment(loanAmount, interestRate, loanTermInYears);
                         Console.WriteLine($"Monthly Repayments: ${Math.Round(monthlyRepayment,2)}");                   
                         Console.WriteLine();
+
+                        //Append details of loan calculation to loans.txt file
+                        //This file is in the bin folder so will be removed if a clean of the project is performed. For a real system this data 
+                        //would be persisted in a proper data store
+                        using (StreamWriter writer = new StreamWriter("loans.txt", true))
+                        { 
+                            writer.WriteLine(calculator);
+                        }
+
                     }
                     //probably never hit as UI does some valdiation
                     catch (ArgumentOutOfRangeException ex)
@@ -64,19 +75,19 @@ namespace Finance.Client
                     //get input and validate
                     do
                     {
-                        Console.WriteLine("Enter the monthly repayment amount:");
+                        Console.WriteLine("Enter the monthly repayment amount in dollars (e.g. 250):");
                     } while (!Double.TryParse(Console.ReadLine(), out repayment) || repayment <= 0);
 
                     double interestRate;
                     do
                     {
-                        Console.WriteLine("Enter interest rate (%):");
+                        Console.WriteLine("Enter interest rate as a percentage (e.g. 5.2):");
                     } while (!Double.TryParse(Console.ReadLine(), out interestRate) || interestRate <= 0);
 
                     int loanTermInYears;
                     do
                     {
-                        Console.WriteLine("Enter the term of the loan in years:");
+                        Console.WriteLine("Enter the term of the loan in years (e.g. 5):");
                     } while (!Int32.TryParse(Console.ReadLine(), out loanTermInYears) || loanTermInYears <= 0);
 
                     //create new calculator object to call repayment method
@@ -88,6 +99,14 @@ namespace Finance.Client
                         double presentValue = calculator.GetPresentValue(repayment, interestRate, loanTermInYears);
                         Console.WriteLine($"Present Value of the loan: ${Math.Round(presentValue, 2)}");
                         Console.WriteLine();
+
+                        //Append details of loan calculation to loans.txt file
+                        //This file is in the bin folder so will be removed if a clean of the project is performed. For a real system this data 
+                        //would be persisted in a proper data store
+                        using (StreamWriter writer = new StreamWriter("loans.txt", true))
+                        { 
+                            writer.WriteLine(calculator);
+                        }
                     }
                     //probably never hit as UI does some valdiation
                     catch (ArgumentOutOfRangeException ex)
@@ -99,7 +118,20 @@ namespace Finance.Client
                 //display calculations saved to file
                 else if (choice == "3")
                 {
-
+                    try
+                    {   // Open the text file using a stream reader.
+                        using (StreamReader sr = new StreamReader("loans.txt"))
+                        {
+                            // Read the stream to a string, and write the string to the console.
+                            String line = sr.ReadToEnd();
+                            Console.WriteLine(line);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The file could not be read:");
+                        Console.WriteLine(e.Message);
+                    }
                 }
             }
             while (choice != "4");
@@ -112,6 +144,8 @@ namespace Finance.Client
         /// </summary>
         private static void ShowMenu()
         {
+            Console.WriteLine("");
+            Console.WriteLine("=====================================");
             Console.WriteLine("Choose an option from the menu below:");
             Console.WriteLine("=====================================");
             Console.WriteLine("1. Calculate Monthly Loan Repayments");
--- Finance/Finance.Library/FinanceLib.cs
+++ Finance/Finance.Library/FinanceLib.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Finance.Library
 {
@@ -7,14 +8,16 @@ namespace Finance.Library
     /// </summary>
     public class FinanceLib
     {
-        //loan amount
+        //loan amount        
         private double _loanAmount;
         //interest rate as a decimal
         private double _interestRateDecimal;
         //loan term in months
         private int _loanTermMonths;
         //monthly repayments
-        private double _repayment;       
+        private double _repayment;
+        //present value of loan
+        private double _presentValue;
 
         /// <summary>
         /// Calculates the monthly loan repayment
@@ -37,7 +40,9 @@ namespace Finance.Library
             _loanTermMonths = loanTerm * 12;
 
             //monthly repayments calculation
-            return _loanAmount / ((1.0 - (1.0 / Math.Pow((1 + (_interestRateDecimal / 12)), (_loanTermMonths)))) / (_interestRateDecimal / 12));
+            _repayment = _loanAmount / ((1.0 - (1.0 / Math.Pow((1 + (_interestRateDecimal / 12)), (_loanTermMonths)))) / (_interestRateDecimal / 12));
+
+            return _repayment;
         }
 
         /// <summary>
@@ -62,7 +67,22 @@ namespace Finance.Library
             _loanTermMonths = loanTerm * 12;              
 
             //Present Value calculation
-            return _repayment * (1 - Math.Pow((1 + (_interestRateDecimal / 12)), (-1 * _loanTermMonths))) / (_interestRateDecimal / 12);
+            _presentValue =  _repayment * (1 - Math.Pow((1 + (_interestRateDecimal / 12)), (-1 * _loanTermMonths))) / (_interestRateDecimal / 12);
+
+            return _presentValue;            
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder loanDetails = new StringBuilder();
+            loanDetails.AppendLine($"Loan Amount: ${_loanAmount}");
+            loanDetails.AppendLine($"Interest Rate: {_interestRateDecimal*100}%");
+            loanDetails.AppendLine($"Loan Term: {_loanTermMonths/12} years");
+            loanDetails.AppendLine($"Monthly Repayment: ${Math.Round(_repayment,2)}");            
+            loanDetails.AppendLine($"Present Value: ${Math.Round(_presentValue,2)}");
+
+            return loanDetails.ToString();
         }
 
     }
